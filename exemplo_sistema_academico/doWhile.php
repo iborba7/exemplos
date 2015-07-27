a96ef69126678d5c655a2094726100c27fe93f7b
Lucas Borba <lborbati@gmail.com>
2015-07-27 17:04:53 -0300

    commit teste

---
+++ exemplo_sistema_academico/doWhile.php
@@ -0,0 +1,17 @@
+<?php
+
+$grupo = array("Alvaro", "Jhonny", "Letty", "Paul");
+$i=0;
+
+do {
+	
+	$aluno = $grupo[$i];
+	echo $aluno.' <br>';
+	$i++;
+	
+	}
+	
+	while($i<count($grupo));
+	?>
+	
+	
